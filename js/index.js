b362d8f25d7bdb8b1eaccdbdb9ac8b05802733c6
Suchi <>
2019-06-27 22:09:07 +0530

    Complete home page commitied by Sucharita and Abhinab

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function showCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+  }
+  
+  function hideCreatePostModal(){
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+  }
+  
+  function openBlogList(){
+    location.href = "/html/bloglist.html"
+  }
